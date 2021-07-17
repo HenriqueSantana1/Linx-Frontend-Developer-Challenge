42fc0e6252ac4ddcbf0e8a6adda4dcc91bfaef8a
Henrique <henrique.santana@ecomp.ufsm.br>
2021-07-17 14:58:31 -0300

    feat: add validate form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-let url = 'https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1'
 const abstract = document.getElementsByClassName('abstract-text')
 const btnCollapse = document.querySelector('.iconCollapseAbstract')
 const productsCards = document.querySelector('.products-cards')
 const btnMoreProducts = document.querySelector(".btnProducts")
+const btnSend = document.querySelector('#btnSend')
+const btnSendFriend = document.querySelector('#btnSendFriend')
+
+let url = 'https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1'
 let isVisible = true;
 
+
 btnCollapse.addEventListener("click", e => {
+    e.preventDefault()
     if (isVisible) {
         abstract[0].classList = 'abstract-text text-invisible'
         abstract[1].classList = 'abstract-text text-invisible'
@@ -22,6 +27,7 @@ btnCollapse.addEventListener("click", e => {
 })
 
 btnMoreProducts.addEventListener("click", e => {
+    e.preventDefault()
     getProducts()
 })
 
@@ -79,9 +85,80 @@ function getProducts() {
     })
 }
 
+btnSend.addEventListener("click", e => {
+    e.preventDefault()
+    let name = document.querySelector('#name')
+    let email = document.querySelector('#email')
+    let cpf = document.querySelector('#cpf')
+    let masc = document.querySelector('#masc')
+    let fem = document.querySelector('#fem')
+    let gen
+    
+    if (!name.value) {
+        alert('Insira seu nome')
+        return false;
+    }
+    else if (!email.value) {
+        alert('Insira seu e-mail')
+        return false;
+    }
+    else if (!isEmail(email.value)) {
+        alert('E-mail inválido')
+        return false;
+    }
+    else if (!cpf.value) {
+        alert('Insira seu CPF')
+        return false;
+    }
+    else if (cpf.value.length < 11) {
+        alert('CPF inválido')
+        return false;
+    }
+    
+    if (masc.checked) {
+        gen = 'Masculino'
+    }
+    else if (fem.checked) {
+        gen = 'Feminino'
+    }
+    else {
+        gen = 'Outro'
+    }
+
+    console.log('Nome: '+name.value+'\nE-mail: '+email.value+'\nCPF: '+cpf.value+'\nGênero: '+gen)
+})
+
+btnSendFriend.addEventListener("click", e => {
+    e.preventDefault()
+    let name = document.querySelector('#fname')
+    let email = document.querySelector('#femail')
+
+    if (!name.value) {
+        alert('Insira seu nome')
+        return false;
+    }
+    else if (!email.value) {
+        alert('Insira seu e-mail')
+        return false;
+    }
+    else if (!isEmail(email.value)) {
+        alert('E-mail inválido')
+        return false;
+    }
+
+    return sendEmail(name.value, email.value)
+})
+
+function isEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function sendEmail(name, email) {
+    console.log('Nome: '+name+'\nE-mail: '+email)
+}
+
 getProducts()
-
-
 
 
 
